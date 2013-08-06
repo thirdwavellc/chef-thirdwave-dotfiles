267c410cfa80203d3058d332e79da4f2b337a11b
Adam Krone <krone.adam@gmail.com>
2013-08-06 11:09:13 -0500

    Revamps attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,25 @@
-# Aliases
-default['thirdwave-dotfiles']['aliases']['liferay'] = false
+#
+# Cookbook Name:: thirdwave-dotfiles
+# Attributes:: default
+#
+# Copyright (C) 2013 Thirdwave, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# ZSH
-default['thirdwave-dotfiles']['zsh'] = false
+default['thirdwave_dotfiles']['user'] = "vagrant"
+default['thirdwave_dotfiles']['user_home'] = "/home/vagrant"
+
+default['thirdwave_dotfiles']['prompt'] = false
+default['thirdwave_dotfiles']['vim'] = false
+default['thirdwave_dotfiles']['zsh'] = false
